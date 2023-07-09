7db653fc0b49915ec5cc81e1c12004c8af374edb
EPILICE <epilice.inc@gmail.com>
2023-07-10 00:46:13 +0900

    baby girl

--- web/weather.js
+++ web/weather.js
@@ -1,11 +1,16 @@
 const COORDS = 'coords'
-const timeData = document.querySelector("time");
-//const apiKey = 'NpzVsuBpaZTQUFcfKqEl%2FUx5ju8A6%2F5LtJCh755PYgXBCuVZxa8d706fMWgEKRvHSe5xTo%2BKNE%2F6g6eiDXRD4w%3D%3D';
+const timeData = document.querySelector(".time");
+const dateDate = document.querySelector(".date")
 const currentWeatherItem = document.querySelector("current-weather-item");
 const timezone = document.getElementById('time-zone');
 const country = document.getElementById('country');
 const weatherForcase = document.getElementById('weather');
 const currentTemp = document.getElementById('current-temp');
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+const API_KEY = 'b60326f54e1c29899d024f74aad3212b';
 
 setInterval(() => {
     const time = new Date();
@@ -19,14 +24,36 @@ setInterval(() => {
     console.log(hour);
     console.log(minutes);
     console.log(ampm);
+    
     timeData.innerHTML = hour12Format + ':' + minutes+ ' ' + `<span id="am-pm">${ampm}</span>`
+
+    dateDate.innerHTML = days[day] + ', ' + date + ' ' + months[month]
+
 }, 1000);
 
 
-// function getweather(){
+getweather()
 
-//     fetch(`http://apis.data.go.kr/1360000/AsosDalyInfoService`).then(function(res))
+function getweather() {
 
+    navigator.geolocation.getCurrentPosition((success) => { //getCurrentPosition은 success콜백만을 취급한다. 성공하면 => 다음행위를 실행함
 
+        console.log(success);
+
+        let {latitude, longitude} = success.coords;  //조금더 공부해야함.
+
+     
     
-// }
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=
+        ${latitude}&lon=${longitude}&appid=
+        ${API_KEY}`).then(res => res.json()).then(data => {
+
+            console.log(data)
+            
+        })
+
+    })
+    
+   
+
+}
